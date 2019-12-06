0dc399eee448559a6303a9f4c176b866a92d33c4
soy-software <soysoftware@gmail.com>
2019-12-06 09:58:30 -0500

    snappy pdf

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,8 @@ return [
     
     'pdf' => [
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltopdf',
+        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        // 'binary'  => '/usr/local/bin/wkhtmltopdf',
         'timeout' => false,
         'options' => [],
         'env'     => [],
@@ -43,7 +44,8 @@ return [
     
     'image' => [
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltoimage',
+        'binary' => base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64'),
+        // 'binary'  => '/usr/local/bin/wkhtmltoimage',
         'timeout' => false,
         'options' => [],
         'env'     => [],
